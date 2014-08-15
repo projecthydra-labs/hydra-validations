5be9e8999ac01440a35b20430e1de9f08dff78dc
dchandekstark <dchandekstark@gmail.com>
2014-08-15 14:09:09 -0400

    Factored out helper methods

--- lib/hydra/validations.rb
+++ lib/hydra/validations.rb
@@ -1,13 +1,12 @@
 require "hydra/validations/version"
+require "hydra/validations/helper_methods"
 
 module Hydra
   module Validations
     extend ActiveSupport::Concern
 
-    module ClassMethods
-      def validates_single_cardinality_of *attr_names
-        validates_with SingleCardinalityValidator, _merge_attributes(attr_names)
-      end
+    included do
+      include HelperMethods
     end
 
   end
--- lib/hydra/validations/helper_methods.rb
+++ lib/hydra/validations/helper_methods.rb
@@ -1,9 +1,12 @@
 module Hydra
   module Validations
     module HelperMethods
+      extend ActiveSupport::Concern
 
-      def validates_single_cardinality_of(*attr_names)
-        validates_with SingleCardinalityValidator, _merge_attributes(attr_names)
+      module ClassMethods
+        def validates_single_cardinality_of *attr_names
+          validates_with SingleCardinalityValidator, _merge_attributes(attr_names)
+        end
       end
 
     end
